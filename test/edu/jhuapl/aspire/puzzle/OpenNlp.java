4dda5aa22ae6994d27ae2c82aa8a3782848d4e2b
matzej1 <matzej1@ITSD-4LZJMC2-WL.dom1.jhuapl.edu>
2020-11-19 16:48:14 -0500

    Tokenizers

--- test/edu/jhuapl/aspire/puzzle/OpenNlp.java
+++ test/edu/jhuapl/aspire/puzzle/OpenNlp.java
@@ -1,16 +1,28 @@
 package edu.jhuapl.aspire.puzzle;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import opennlp.tools.tokenize.*;
 
 public class OpenNlp {
-	OpenNlp() throws IOException{getResourcesAsStream("models/en-token.bin");
-	InputStream inputStream = null;
-	TokenizerModel model = new TokenizerModel(inputStream);
-	Tokenizer tokenizer = new TokenizerME(model);
-	tokenizer.tokenize("Hanna lives in the red house.");
-	}
-	public void getResourcesAsStream(String stringstring) {
+	@Test
+	public void sentenceTester() throws IOException {
+		InputStream inputStream = new FileInputStream("en-token.bin");
+		TokenizerModel model = new TokenizerModel(inputStream);
+		Tokenizer tokenizer = new TokenizerME(model);
+		tokenizer.tokenize("Hanna lives in the red house.");
+		String[] tokens;
+		tokens = tokenizer.tokenize("Hanna lives in the red house.");
+		Assertions.assertEquals(tokens.length, 7);
+		System.out.println(Arrays.toString(tokens));
+		StringTokenizer tokenizer2 = new StringTokenizer("Hanna lives in the red house.");
+		System.out.println(tokenizer2.countTokens());
 	}
 }
